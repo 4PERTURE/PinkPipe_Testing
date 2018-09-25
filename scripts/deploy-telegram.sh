ad24ac8f50fc4dcad4ee50e1188fddee384b9458
4PERTURE <38117317+4PERTURE@users.noreply.github.com>
2018-09-25 10:22:10 +0100

    Fix rart

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 #cp app/build/outputs/apk/release/app-release.apk PinkPipe-$TRAVIS_BUILD_NUMBER.apk
 curl -F chat_id="-1001289629950" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
+curl -F chat_id="-1001289629950" -F document=@"PinkPipe-$TRAVIS_BUILD_NUMBER.apk" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 
 echo $(./scripts/changelog.sh)
