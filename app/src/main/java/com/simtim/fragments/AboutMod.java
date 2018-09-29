691adfbbc289c0f8a2b8a77f41f55ca49b9a7325
deletescape <me@deletescape.ch>
2018-09-29 12:03:18 +0530

    I need help

--- app/src/main/java/com/simtim/fragments/AboutMod.java
+++ app/src/main/java/com/simtim/fragments/AboutMod.java
@@ -21,19 +21,38 @@ import android.support.annotation.Nullable;
 import org.schabi.newpipe.settings.BasePreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.widget.Toolbar;
+
 import me.jfenn.attribouter.Attribouter;
 
-public class AboutMod extends AppCompatActivity {
+public class AboutMod extends AppCompatActivity implements PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
         @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-     
-    @Override
-     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {  
-                Attribouter.from(context)
-                .withFile(R.xml.about_mod)
-                .show();
+
+     public void onCreatePreferences(Bundle bundle, String s) {  
+         addPreferencesFromResource(getContent());
     }
- }
+       @Override
+        public boolean onPreferenceTreeClick(Preference preference) {
+               ((SettingsActivity) getActivity()).onPreferenceStartFragment(this, preference);
+                return true;
+                return super.onPreferenceTreeClick(preference);
+        }
+     @Override
+    public boolean onPreferenceStartFragment(PreferenceFragmentCompat caller, Preference pref) {
+        Fragment fragment;
+        fragment = Attribouter.from(this).withFile(R.xml.about_mod).toFragment(); 
+        return false;
+}
