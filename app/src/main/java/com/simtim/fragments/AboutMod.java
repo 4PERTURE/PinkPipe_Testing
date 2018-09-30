0bf5c74b64ad425dd7e65aafbc726b3c02877548
Simrat Singh <simrats169169@gmail.com>
2018-09-30 14:18:05 +0530

    kek

--- app/src/main/java/com/simtim/fragments/AboutMod.java
+++ app/src/main/java/com/simtim/fragments/AboutMod.java
@@ -9,36 +9,59 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
-implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package com.simtim.fragments;
 
-import org.schabi.newpipe.R;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import org.schabi.newpipe.settings.BasePreferenceFragment;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import org.schabi.newpipe.R;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.schabi.newpipe.R;
+import android.support.annotation.Nullable;
 
 import me.jfenn.attribouter.Attribouter;
 
-public class AboutMod extends AppCompatActivity {
+import org.schabi.newpipe.settings.BasePreferenceFragment;
 
-        @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class AboutMod extends BasePreferenceFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
     }
-         @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-                 Attribouter.from(this)
-                    .withFile(R.xml.about_mod)
-                    .show();
-        return super.onOptionsItemSelected(item);
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    //    super.onCreate(savedInstanceState);
+        //addPreferencesFromResource(R.xml.about_mod);
+            Attribouter.from(this)
+                .withFile(R.xml.about)
+                .show();        
     }
-}
+ }
