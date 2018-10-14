9584f1107a5a8eb285f98fded6d9b070c6cb3d55
Simrat Singh <simrats169169@gmail.com>
2018-10-14 20:44:30 +0530

    Reserve conflicts and cleanip imports

--- app/src/main/java/com/simtim/fragments/AboutMod.java
+++ app/src/main/java/com/simtim/fragments/AboutMod.java
@@ -17,58 +17,12 @@ implied.
 package com.simtim.fragments;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
-<<<<<<< HEAD
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-
-import me.jfenn.attribouter.Attribouter;
-
-import static me.jfenn.attribouter.Attribouter.*;
-
-public class AboutMod extends Fragment {
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        final Attribouter attribouter;
-        attribouter = from(getActivity());
-        Attribouter.from(getActivity()).withFile(R.xml.about_mod);
-        Attribouter.from(getActivity()).show();
-=======
-import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v14.preference.SwitchPreference;
-import android.provider.Settings;
+import android.view.Surface;
+import android.preference.Preference;
 import org.schabi.newpipe.R;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import org.schabi.newpipe.BuildConfig;
-import org.schabi.newpipe.R;
-import org.schabi.newpipe.util.NavigationHelper;
-import org.schabi.newpipe.util.ThemeHelper;
-import org.schabi.newpipe.util.Constants;
-import android.support.annotation.Nullable;
-
 import org.schabi.newpipe.settings.BasePreferenceFragment;
 
 public class AboutMod extends BasePreferenceFragment {
@@ -82,28 +36,5 @@ public class AboutMod extends BasePreferenceFragment {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
     //    super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.about_mod);
-        
->>>>>>> parent of da8132b4... Initial About Mod page
     }
-
-    /**@Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-    final Attribouter attribouter;
-    attribouter = from(getActivity());
-    from(getActivity()).withFile(R.xml.about_mod);
-    from(getActivity()).show();
-
-    }**/
-
-
-    /** @Override
-    protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    Attribouter.from(this)
-    .withFile(R.xml.about_mod)
-    .show();
-    }**/
 }
