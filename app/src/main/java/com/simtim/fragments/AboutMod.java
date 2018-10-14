dd8a01416d690faab96306c93fcc7f0c0871451a
4PERTURE <slicey2001@gmail.com>
2018-10-14 15:54:57 +0100

    lets start over

--- app/src/main/java/com/simtim/fragments/AboutMod.java
+++ app/src/main/java/com/simtim/fragments/AboutMod.java
@@ -16,8 +16,10 @@ implied.
  */
 package com.simtim.fragments;
 
-import org.schabi.newpipe.R;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+<<<<<<< HEAD
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
@@ -35,6 +37,53 @@ public class AboutMod extends Fragment {
         attribouter = from(getActivity());
         Attribouter.from(getActivity()).withFile(R.xml.about_mod);
         Attribouter.from(getActivity()).show();
+=======
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import org.schabi.newpipe.R;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.schabi.newpipe.BuildConfig;
+import org.schabi.newpipe.R;
+import org.schabi.newpipe.util.NavigationHelper;
+import org.schabi.newpipe.util.ThemeHelper;
+import org.schabi.newpipe.util.Constants;
+import android.support.annotation.Nullable;
+
+import org.schabi.newpipe.settings.BasePreferenceFragment;
+
+public class AboutMod extends BasePreferenceFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    //    super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.about_mod);
+        
+>>>>>>> parent of da8132b4... Initial About Mod page
     }
 
     /**@Override
